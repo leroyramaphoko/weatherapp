2e4ac5627a7cea9c145848690445554e6b6d0855
Leroy Ramaphoko <leroyramaphoko@gmail.com>
2021-06-29 09:29:36 +0200

    ✓ Targeted SDK 29 ✓ Fixed the date format

--- app/src/main/java/com/dvt/weatherapp/common/enums/DateFormat.kt
+++ app/src/main/java/com/dvt/weatherapp/common/enums/DateFormat.kt
@@ -2,5 +2,5 @@ package com.dvt.weatherapp.common.enums
 
 enum class DateFormat(val format: String) {
     DAY_IN_FULL("EEEE"),
-    DD_MMM_YYYY_HH_MM_AA("dd MMM YYYY HH:MM aa")
+    DD_MMM_YYYY_HH_MM_AA("dd MMM YYYY hh:mm aa")
 }
